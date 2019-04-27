a0434305252db34b51527d3acc21eb325d201e09
Ryan Nguyen <contactryannguyen@gmail.com>
2019-04-27 12:03:54 -0400

    Removed unnecessary dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ const gulp = require('gulp')
 
 const concat = require('gulp-concat')
 const cleanCSS = require('gulp-clean-css')
-const source = require('vinyl-source-stream')
 const webpack = require('webpack-stream')
 const babel = require('gulp-babel')
 const minify = require('gulp-babel-minify')
